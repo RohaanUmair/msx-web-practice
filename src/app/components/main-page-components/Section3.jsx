f0cc0b550f45b01ad13684eab7d05531256bbef7
RohaanUmair <m.rohaanumair@gmail.com>
2024-12-05 12:51:53 +0500

    commit

--- src/app/components/main-page-components/Section3.jsx
+++ src/app/components/main-page-components/Section3.jsx
@@ -7,11 +7,11 @@ import { FaGears } from "react-icons/fa6";
 
 function Section3() {
     return (
-        <div className="sm:px-24 h-screen w-full flex flex-col items-center justify-center max-sm:h-full">
-            <h1 className="text-purple-500 font-semibold text-base">WHY CHOOSE MSX</h1>
-            <h2 className="text-4xl font-semibold">Why MSX is Your Best Choice</h2>
+        <div className="sm:px-24 h-screen w-full flex flex-col items-center justify-center max-sm:h-full max-sm:pt-5 max-sm:pb-8">
+            <h1 className="text-purple-500 font-semibold text-base max-sm:mb-3">WHY CHOOSE MSX</h1>
+            <h2 className="text-4xl font-semibold max-sm:text-center">Why MSX is Your Best Choice</h2>
 
-            <div className="w-full flex gap-6 mt-14 max-sm:flex-wrap max-sm:gap-0">
+            <div className="w-full flex gap-6 mt-14 max-sm:flex-wrap max-sm:gap-0 max-sm:px-3 max-sm:mt-8">
                 <CardSec3
                     icon={<GrCertificate />}
                     title={'Valuable Certificate'}
